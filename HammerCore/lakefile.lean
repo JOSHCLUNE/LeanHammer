6c72f454d6d50a8dc8ca123d1820fda5d7269bc8
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-05-14 18:21:34 -0400

    Propagate lean-auto update postscript bug fix

--- HammerCore/lakefile.lean
+++ HammerCore/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «aesop» from git "https://github.com/leanprover-community/aesop" @ "v4.20.0-rc2"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "70363959d03d11205efd94d3e1d73e7b2e2c6695"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "9c9e3d2c2b4711c66b19b7e8791fe0e537c7c765"
 
 package HammerCore {
   precompileModules := true
