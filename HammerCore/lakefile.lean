05b42afd04de3fcaf84f0058326ed7607d05f8ff
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-05-14 14:57:26 -0400

    Update Duper dependency

--- HammerCore/lakefile.lean
+++ HammerCore/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «aesop» from git "https://github.com/leanprover-community/aesop" @ "v4.20.0-rc2"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "4bd11f5af780aded58ef038ee05be6b40e6a208f"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "8b40ddbea14dc992f085c568923a58ca1438c5ca"
 
 package HammerCore {
   precompileModules := true
