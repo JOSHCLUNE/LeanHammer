295b9e3ebbebfece997296e07e2e61c61466ab81
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-05-19 18:36:42 -0400

    Propagate updates to make LeanHammer work with Windows

--- HammerCore/lakefile.lean
+++ HammerCore/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «aesop» from git "https://github.com/leanprover-community/aesop" @ "v4.20.0-rc2"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "7acf7e0d4afef7bee340380078fd69736f91cf88"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "5ba6bd2c3693b45b5e8da9cadbab416c2bdff2e8"
 
 package HammerCore {
   precompileModules := true
