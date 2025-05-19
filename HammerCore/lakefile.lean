01f466d3b165ca162004117fff4e25b17eb2937b
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-05-19 19:26:30 -0400

    Propagate lean-auto change

--- HammerCore/lakefile.lean
+++ HammerCore/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «aesop» from git "https://github.com/leanprover-community/aesop" @ "v4.20.0-rc2"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "5ba6bd2c3693b45b5e8da9cadbab416c2bdff2e8"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "08f45c9b00960ee97351ba43cde133933803ab22"
 
 package HammerCore {
   precompileModules := true
