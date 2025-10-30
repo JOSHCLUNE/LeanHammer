cdf6cf580be9bbcdb795fde12e29d4cb6a4fffc0
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-10-30 17:05:44 -0400

    Update inputRevs for dependencies

--- HammerCore/lakefile.lean
+++ HammerCore/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require «aesop» from git "https://github.com/leanprover-community/aesop" @ "v4.24.0"
 
-require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "v4.24.0"
+require «Duper» from git "https://github.com/leanprover-community/duper.git" @ "main"
 
 package HammerCore {
   precompileModules := true
