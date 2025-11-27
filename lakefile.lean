44af27d53f104bed825e5c0d701beb57773d71f6
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-11-27 14:23:48 -0500

    Update to v4.25.1

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,9 @@
 import Lake
 open Lake DSL
 
-require «premise-selection» from git "https://github.com/hanwenzhu/premise-selection" @ "v4.25.0"
+require «premise-selection» from git "https://github.com/hanwenzhu/premise-selection" @ "v4.25.1"
 
-require «Qq» from git "https://github.com/leanprover-community/quote4.git" @ "v4.25.0"
+require «Qq» from git "https://github.com/leanprover-community/quote4.git" @ "v4.25.1"
 
 require «HammerCore» from "./HammerCore"
 
